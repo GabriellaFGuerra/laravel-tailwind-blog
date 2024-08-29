a48ffbf71ff781bafd35e82ccbb654b873e823a7
gabriellafguerra21 <gabriellafguerra21@gmail.com>
2024-08-29 10:55:22 +0000

    Post controller logic

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class PostController extends Controller
 {
@@ -12,7 +14,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+
+        return view('index', $posts);
     }
 
     /**
@@ -20,7 +24,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('createPost');
     }
 
     /**
@@ -28,38 +32,69 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validate = $request->validate(
+            [
+                'title' => 'required',
+                'content' => 'required'
+            ]
+        );
+
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->user_id = Auth::id();
+
+        if (!$post->save()) {
+            return Redirect::back()->withInput($request->all())->withErrors($request->errors());
+        } else {
+            return Redirect::to('post')->with($post);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Post $post)
+    public function show(Post $id)
     {
-        //
+        $post = Post::find($id)->with(['user', 'comments'])->get();
+
+        return view('post', $post);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Post $post)
+    public function edit(Post $id)
     {
-        //
+        $post = Post::find($id)->get();
+
+        return view('editPost', $post);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Post $post)
+    public function update(Request $request, Post $id)
     {
-        //
+        $post = Post::find($id)->get();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+
+        if (!$post->save()) {
+            return Redirect::back()->withInput($request->all())->withErrors($request->errors());
+        } else {
+            return Redirect::to('post')->with($post);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post)
+    public function destroy(Post $id)
     {
-        //
+        $post = Post::find($id)->first();
+        $post->delete();
+
+        return Redirect::to('index');
     }
 }
