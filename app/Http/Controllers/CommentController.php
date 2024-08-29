1e1f2d0a5fdad950e854c205aa0295f0e2066519
gabriellafguerra21 <gabriellafguerra21@gmail.com>
2024-08-29 11:09:59 +0000

    Comment controller logic

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,47 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Comment;
 use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class CommentController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Post $post)
     {
-        //
-    }
+        $comment = new Comment();
+        $comment->comment = $request->input('comment');
+        $comment->post_id = $post;
+        $comment->user_id = Auth::id();
+        $comment->save();
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Comment $comment)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Comment $comment)
-    {
-        //
+        return Redirect::back()->with('comment', $comment);
     }
 
     /**
@@ -52,7 +29,11 @@ class CommentController extends Controller
      */
     public function update(Request $request, Comment $comment)
     {
-        //
+        $comment = Comment::find($comment);
+        $comment->comment = $request->input('comment');
+        $comment->save();
+
+        return Redirect::back()->with('comment', $comment);
     }
 
     /**
@@ -60,6 +41,7 @@ class CommentController extends Controller
      */
     public function destroy(Comment $comment)
     {
-        //
+        Comment::find($comment)->first()->delete();
+        return Redirect::back();
     }
 }
